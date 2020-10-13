bf0add770c977682c0dc8a9a6c2c3a21becc01ff
Lindsay <lindsaynadell89@gmail.com>
2020-10-13 10:09:20 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
